286d452b31243d912c5d53fc7adbf542454a6769
Igor Giussani <gius80@gmail.com>
2023-09-14 14:36:12 +0200

    Make button primary

--- client/my-sites/domains/domain-management/transfer/transfer-page/index.tsx
+++ client/my-sites/domains/domain-management/transfer/transfer-page/index.tsx
@@ -352,6 +352,7 @@ const TransferPage = ( props: TransferPageProps ) => {
 						</b>
 					</p>
 					<RenewButton
+						primary
 						purchase={ purchase }
 						selectedSite={ selectedSite }
 						subscriptionId={ parseInt( domain?.subscriptionId ?? '', 10 ) }
